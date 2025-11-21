ecf8819e8c67393dde76b5d30f231214e14ec2ad
Arunchhibber <arunchhibber0101@gmail.com>
2025-11-20 21:40:43 -0500

    corrected the alignment of the screen

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import Game from "./Game";
 
-const PLAYER_SIZE = 30;
-
-const Player = ({ x, bottom }) => {
+function App() {
   return (
     <div
       style={{
-        position: "absolute",
-        width: PLAYER_SIZE,
-        height: PLAYER_SIZE,
-        backgroundColor: "red",
-        left: x,
-        bottom: bottom,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", // full screen vertically
+        background: "#f0f0f0", // optional background
       }}
-    />
+    >
+      <div>
+        <h1 style={{ textAlign: "center" }}>Jump Game with Dual Obstacles</h1>
+        <Game />
+      </div>
+    </div>
   );
-};
+}
 
-export default Player;
+export default App;
